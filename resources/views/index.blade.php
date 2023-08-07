087e495aea80848b96f071bbb596929d338ea85c
Dmytro Tiulpa <dmytrotiulpa@gmail.com>
2023-08-07 10:30:01 +0300

    add modal

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,14 +5,78 @@
 @section('keywords', 'myKeyword1, myKeyword2, myKeyword3')
 
 @section('content')
-    <div class="uk-section uk-section-small uk-section-muted" style="padding-top: 20px;"> {{--height: 100vh;--}}
+    <div class="uk-section uk-section-small uk-section-muted" style="height: 100vh;">
         <div class="uk-container uk-container-xlarge uk-margin-bottom">
 
             @php
                 var_dump($_POST);
             @endphp
 
-            <div class="uk-flex uk-flex-center uk-margin-bottom">
+            <a class="uk-button uk-button-secondary uk-margin-bottom" href="#modal-center" data-uk-toggle>Добавить коментарий</a>
+
+            <div id="modal-center" class="uk-flex-top" data-uk-modal>
+                <div class="uk-modal-dialog uk-modal-body uk-margin-auto-vertical">
+
+                    <button class="uk-modal-close-default" type="button" data-uk-close></button>
+
+                    <form method="post" action="">
+                        @csrf
+                        <fieldset class="uk-fieldset">
+
+                            <legend class="uk-legend">Добавить коментарий</legend>
+
+                            <div class="uk-margin">
+                                <div class="uk-inline uk-width-1-1">
+                                    <span class="uk-form-icon" data-uk-icon="icon: user"></span>
+                                    <input name="user_name" class="uk-input" type="text"
+                                           placeholder="User name" aria-label="Input"
+                                           required >
+                                </div>
+                            </div>
+
+                            {{--@error('email')
+                            <div class="uk-alert-danger uk-margin-small" data-uk-alert>
+                                <a class="uk-alert-close" data-uk-close></a>
+                                <p>{{ $message }}</p>
+                            </div>
+                            @enderror--}}
+
+                            <div class="uk-margin">
+                                <div class="uk-inline uk-width-1-1">
+                                    <span class="uk-form-icon" data-uk-icon="icon: mail"></span>
+                                    <input name="email" class="uk-input" type="text"
+                                           placeholder="E-mail" aria-label="E-mail"
+                                           required >
+                                </div>
+                            </div>
+
+                            <div class="uk-margin">
+                                <div class="uk-inline uk-width-1-1">
+                                    <span class="uk-form-icon" data-uk-icon="icon: link"></span>
+                                    <input  name="home_page" class="uk-input" type="text"
+                                            placeholder="Home page" aria-label="Home page">
+                                </div>
+                            </div>
+
+                            <div class="uk-margin">
+                            <textarea class="uk-textarea" rows="5" placeholder="Textarea" aria-label="Textarea"
+                                      required></textarea>
+                            </div>
+
+                            <div class="uk-margin">
+                                <button class="uk-button uk-button-secondary uk-width-1-1"
+                                        type="submit">
+                                    Сохранить
+                                </button>
+                            </div>
+
+                        </fieldset>
+                    </form>
+
+                </div>
+            </div>
+
+            {{--<div class="uk-flex uk-flex-center uk-margin-bottom">
                 <div class="uk-card uk-card-default uk-card-body uk-width-1-2">
                     <form method="post" action="">
                         @csrf
@@ -29,12 +93,12 @@
                             </div>
                         </div>
 
-                        {{--@error('email')
+                        --}}{{--@error('email')
                         <div class="uk-alert-danger uk-margin-small" data-uk-alert>
                             <a class="uk-alert-close" data-uk-close></a>
                             <p>{{ $message }}</p>
                         </div>
-                        @enderror--}}
+                        @enderror--}}{{--
 
                         <div class="uk-margin">
                             <div class="uk-inline uk-width-1-1">
@@ -68,7 +132,7 @@
                     </fieldset>
                     </form>
                 </div>
-            </div>
+            </div>--}}
 
             <div class="uk-card uk-card-default uk-width-1-1">
                 <div class="uk-card-header">
@@ -79,6 +143,9 @@
                         <div class="uk-width-expand">
                             <h3 class="uk-card-title uk-margin-remove-bottom">Title</h3>
                             <p class="uk-text-meta uk-margin-remove-top"><time datetime="2016-04-01T19:00">April 01, 2016</time></p>
+                        </div>
+                        <div class="uk-width-auto">
+                            <a class="uk-button uk-button-text" href="#modal-center" data-uk-toggle>Ответить</a>
                         </div>
                     </div>
                 </div>
